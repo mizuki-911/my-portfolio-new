fdffdaa1b34bf5a94d1773d8e58c57045789232d
mizuki-911 <mizuki-matsuzaki@docomo.ne.jp>
2025-03-04 15:10:32 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const lastName='右寺';
+const firstName='隆信';
+const birthyear='1982';
+const hometown='佐賀'
+
+const greeting=`私の名前は${lastName}${firstName}です。
+${birthyear}年生まれで、${hometown}出身です。`;
+console.log(greeting);
